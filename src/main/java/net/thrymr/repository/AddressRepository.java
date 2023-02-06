174be71c1f4c9bd249cea35fda116229ba56e87c
MandlaMamatha <124438186+MandlaMamatha@users.noreply.github.com>
2023-02-06 10:56:23 +0530

    Delete AddressRepository.java

--- src/main/java/net/thrymr/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package net.thrymr.repository;
-
-import net.thrymr.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address,Long> {
-}
